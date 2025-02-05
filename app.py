65e6237fa6df0552e6019e2afb54922ff8910b98
tlwong2005 <42258433+tlwong2005@users.noreply.github.com>
2025-02-05 15:22:41 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import openai
+
+openai.api_key =  st.secrets["mykey"]
+
+# Title and Heading
+st.title("My First Streamlit App")
+st.header("This is a header")
+st.write("This is some text.")
+
+# Input and Output
+name = st.text_input("Enter your name:", value="Type here")
+if st.button("Submit"):
+    st.write(f"Hello, {name}! I love TAR UMT! ")
